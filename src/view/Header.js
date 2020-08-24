926bf55f01a9accca841d7d6f8cd712ac9dde66e
AndrasNemeth94 <nemethand94@gmail.com>
2020-08-24 19:37:09 +0200

    Home Page(basic structure) v2

--- src/view/Header.js
+++ src/view/Header.js
@@ -9,37 +9,65 @@ class Header extends Component {
         return (
             <div className='container-fluid'>{/*----container--- */}
 
-                <div className='row'>{/*----row----- */}
 
-                        <div className='col-sm logo'>{/*----logo---- */}
-                                <p>logo</p>
-                        </div>{/*----logo end---- */}
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">{/*----navbar----- */}
 
-                        <div className='col-sm menu'>{/*----col 1---- */}
-                                <p>menu</p>
-                        </div>{/*----col 1 end---- */}
+                <div className='row menuRow'>{/*---menuRow---- */}
 
-                        <div className='col-sm searchBar'>{/*----col 2---- */}
-                                <p>Searchbar</p>
-                        </div>{/*----col 2 end---- */}
-                                
-                        <div className='col-sm pro'>{/*----col 3---- */}
-                                    <p>IMDbPro</p>
-                        </div>{/*----col 3 end---- */}
+                       <div className='col-sm-4 logoCol'> {/*---logoCol---- */}
+                        <a className="navbar-brand" href="Home">{/*-----logo link----- */}
+                                <img src= {require('../img/LoMLM.png')} alt='LoMLM' width='200px' height='60px' />
+                        </a>{/*-----logo link end----- */}
 
-                        <div className='col-sm watchList'>{/*----col 4---- */}
-                                <p>Watch List</p>
-                        </div>{/*----col 4 end---- */}
+                        </div>{/*---logoCol end---- */}
 
-                        <div className='col-sm signIn'>{/*----col 5---- */}
-                                <p>Sign in</p>
-                        </div>{/*----col 5 end---- */}
+                        <div className='col-sm-4'></div>{/*------EMPTY COL----- */}
 
-                        <div className='col-sm'>{/*----col 6---- */}
-                                
-                        </div>{/*----col 6 end---- */}
-                    </div>{/*----row end----- */}
-            {/*----container end--- */}
+                        <div className='col-sm-6 menuCol'>{/*---menuCol----- */}
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">{/*--hamburger menu button--- */}
+                                <span className="navbar-toggler-icon"></span>
+                        </button>{/*--hamburger menu button end--- */}
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">{/*---collapsed menu container--- */}
+
+                        <ul className="navbar-nav navlinkList">{/*---dropdown menu---- */}
+
+                                <li className="nav-item">
+
+                                        <a className="nav-link" href="Home2">
+                                                <p className='linkText'> HOME</p>
+                                         </a>
+                                </li>
+
+                                <li className="nav-item">
+
+                                        <a className="nav-link" href="Home2">
+                                                <p className='linkText'> PROGRAMS</p>
+                                         </a>
+                                </li>
+
+                                <li className="nav-item">
+
+                                        <a className="nav-link" href="Home2">
+                                                <p className='linkText'> BLOG</p>
+                                         </a>
+                                </li>
+
+                                <li className="nav-item">
+
+                                        <a className="nav-link" href="Home2">
+                                               <p className='linkText'> CONTACT</p>
+                                         </a>
+                                </li>
+
+                        </ul>{/*---dropdown menu end---- */}
+
+                        </div>{/*---collapsed menu container end--- */}
+
+                        </div>{/*---menuCol end----- */}
+                </div>{/*---menuRow end---- */}
+                </nav>
             </div>
         );
     }
